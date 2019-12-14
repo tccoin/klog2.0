c3c5b35e5c76a2e79db17a28d186a6966493ff4e
tccoin <1774241372@qq.com>
2019-12-15 01:09:53 +0800

    put server scripts into klog/

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'klog',
-    script: 'server.js',
+    script: 'klog/server.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: '',
--- settings.js
+++
@@ -1,7 +0,0 @@
-const appId = 'nna3GjtCul4qbfaIofuhDtE8-MdYXbMMI';
-const appKey = 'L37rInrbeo0nVMtwFqG9yC50';
-const buildDirectory = 'build';
-
-exports.appId = appId;
-exports.appKey = appKey;
-exports.buildDirectory = buildDirectory;
