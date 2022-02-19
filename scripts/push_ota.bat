b54257371095e0877c1d675fa66064a916778787
Link Wu <1774241372@qq.com>
2022-02-19 01:30:13 -0500

    update tools

--- scripts/push_ota.bat
+++ scripts/push_ota.bat
@@ -1,4 +1,4 @@
-bc c -r build/build.zip build/*
+bz c -r build/build.zip build/*
 ssh klog "rm -rf /root/klog2.0/build/*"
 sftp -b scripts/sftp.txt klog
-ssh klog "unzip /root/klog2.0/build/build.zip -d /root/klog2.0/build"
+ssh klog "unzip /root/klog2.0/build.zip -d /root/klog2.0"
