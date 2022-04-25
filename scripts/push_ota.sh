0a9407225c22e53ca47ad1477913aabeb34cc071
tccoin <coin12.wu@gmail.com>
2022-04-25 03:13:11 -0400

    update scripts

--- scripts/push_ota.sh
+++ scripts/push_ota.sh
@@ -1,4 +1,4 @@
-zip -r build/build.zip build
+zip -rq build/build.zip build
 ssh klog "rm -rf /root/klog2.0/build/*"
 sftp -b scripts/sftp.txt klog
 ssh klog "unzip /root/klog2.0/build.zip -d /root/klog2.0"
