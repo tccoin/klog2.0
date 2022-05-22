97c9294efcbbe9002993e36e4fda446362327da9
Link Wu <coin12.wu@gmail.com>
2022-05-22 01:15:28 -0400

    update toast border radius

--- src/style/klog-style-toast.js
+++ src/style/klog-style-toast.js
@@ -3,6 +3,9 @@ const containerKlogStyleToast = document.createElement('template');
 containerKlogStyleToast.innerHTML = `<dom-module id="klog-style-toast">
   <template>
     <style>
+      paper-toast {
+        border-radius: 5px;
+      }
       paper-toast a {
         color: var(--paper-yellow-500);
         float: right;
